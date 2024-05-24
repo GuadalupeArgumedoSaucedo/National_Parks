cf55ccd8bb1c57dec19e2822a0a35a1cc432c3eb
GuadalupeArgumedoSaucedo <lupeargumedo96@gmail.com>
2024-05-24 07:07:05 -0700

    final commit

--- JavaScript/mountains.js
+++ JavaScript/mountains.js
@@ -1,99 +1,81 @@
-import {mountainsArray} from "./mountainData";
+"use strict";
 
-const moutainHeader = document.getElementById("mountain-header");
-const mountainDropdown = document.getElementById("mountain-dropdown");
-const mountainDropdownlabel = document.getElementById("mountain-dropdown-label");
+// Import mountain data array
+import { mountainsArray } from "./mountainData.js";
+
+// DOM element references
 const mountainDropdownSelect = document.getElementById("mountain-dropdown-select");
 const searchButton = document.getElementById("search-button");
-const mountainResults = document.getElementById("mountain-results");
 const myMountains = document.getElementById("my-mountains");
 
-mountainDropdownSelect.addEventListener("change", updateDropdown);
-searchButton.addEventListener("click", () => {
-  let filteredMountains = [];
-filteredMountains = mountainsArray.filter(mountain => mountain.name ===mountains)
-displayResults(filteredMountains);
-});
-
-populateDropdown(mountainDropdownSelect,mountainsArray);
-
-
-
-
-
+// Function to populate dropdown menu
 function populateDropdown(inputSearch, dataArray) {
-  inputSearch.innerHTML = ""; // Clear existing options
-  mountainsArray.forEach(item => {
-    const option = document.createElement("option");
-    option.value = item;
-    option.text = item;
-    inputSearch.appendChild(option);
-    if(option.index == 0){
-      option.selected = 'true';
-      getMountains(item);
-    }
+    inputSearch.innerHTML = ""; 
+    dataArray.forEach(item => {
+        const option = document.createElement("option");
+        option.value = item.name; // Setting option value to mountain name
+        option.text = item.name; // Setting option text to mountain name
+        inputSearch.appendChild(option);
+    });
+}
+
+// Event listener for search button click
+searchButton.addEventListener("click", () => {
+    // Filtering mountains based on selected mountain name
+    const selectedMountain = mountainDropdownSelect.value;
+    const filteredMountains = mountainsArray.filter(mountain => mountain.name === selectedMountain);
+    // Displaying filtered mountains
+    myMountains.innerHTML = filteredMountains.map(mountainTemplate).join('');
 });
-} 
 
-function searchOptions(){
-  populateDropdown(searchInput,locationArray);
-  mountainTemplat(inputSearch.value);
-}
+// Populating dropdown menu (when DOM content is loaded)
+document.addEventListener("DOMContentLoaded", function() {
+    populateDropdown(mountainDropdownSelect, mountainsArray);
+});
 
-function updateDropdown(){
-  getMountains(searchInput.value);
-
-
-function getMountains(selectedItem){
-  myMountains.innerHTML = "";
-  mountainsArray.forEach(item => {
-    mountainTemplat(item)
-  })
-}
-
-
-
+// Function to generate card for mountain
 function mountainTemplate(mountain) {
-    //let mySunrise = getSunsetForMountain(`${mountain.coords.lat},${mountain.coords.lng}`);
-  
-    return `
+  // Fetching sunrise data for the mountain
+  //let mySunrise = getSunsetForMountain(mountain.coords.lat, mountain.coords.lng);
+  return `
       <div class="mountain">
-      <img class="mountain-photo" src="images/${mountain.img}">
-      <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
-      <h4 class="mountain-desc">${mountain.desc} </h4>
-      <p><strong>Effort:</strong> ${mountain.effort}</p>
-      <strong>Coordinates:</strong> lat: ${mountain.coords.lat} lng: ${mountain.coords.lng}
-      <strong>Sunrise:</strong> ${mySunrise}
+          <img class="mountain-photo" src="/images/${mountain.img}" alt="${mountain.name}">
+          <h2 class="mountain-name">${mountain.name} <span class="species">(${mountain.elevation} feet)</span></h2>
+          <h4 class="mountain-desc">${mountain.desc}</h4>
+          <p><strong>Effort:</strong> ${mountain.effort}</p>
+          <p><strong>Coordinates:</strong> lat: ${mountain.coords.lat}, lng: ${mountain.coords.lng}</p>
       </div>
-      `;
-  }
-  
-
-
-  document.getElementById("mountains").innerHTML = `
-    <h1 class="app-title"> ${mountainsArray.length} Mountains to climb</h1>
-    ${mountainsArray.map(mountainTemplate).join("")}
-    <p class="footer">These ${
-      mountainsArray.length
-    } mountants were added recently. Check back soon for updates.</p>
   `;
-   
-  async function getSunsetForMountain(lat, lng) {
-    let response = await fetch(
-      `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lng}&date=today`
-    );
-    let data = await response.json();
-    return data;
+}
+/*
+<strong>Sunrise:</strong> ${mySunrise}
+
+// Displaying total number of mountains and mountain list in the HTML
+document.getElementById("mountains").innerHTML = `
+  <h1 class="app-title"> ${mountainsArray.length} Mountains to climb</h1>
+  ${mountainsArray.map(mountainTemplate).join("")}
+  <p class="footer">These ${
+    mountainsArray.length
+  } mountains were added recently. Check back soon for updates.</p>
+`;
+
+// Function to fetch sunrise data for a given location (lat, lng)
+async function getSunsetForMountain(lat, lng) {
+  let response = await fetch(`https://api.sunrise-sunset.org/json?lat=36.7201600&lng=-4.4203400&date=today`);
+  let data = await response.json();
+  return data;
+}
+
+// Function to load mountain data into dropdown menu
+function loadData() {
+  var down = document.getElementById("mountain");
+  for (let i = 0; i < mountainsArray.length; i++) {
+    var optn = mountainsArray[i];
+    var el = document.createElement("option");
+    el.textContent = optn;
+    el.value = optn;
+    down.appendChild(el);
   }
-  
-  function loadData() {
-    var down = document.getElementById("mountain");
-    for (let i = 0; i < mountainsArray.length; i++) {
-      var optn = mountainsArray[i];
-      var el = document.createElement("option");
-      el.textContent = optn;
-      el.value = optn;
-      down.appendChild(el);
-    }
-    down.innerHTML = "Elements Added";
-  }}
+  down.innerHTML = "Elements Added";
+}
+*/
--- JavaScript/nationalParks.js
+++ JavaScript/nationalParks.js
@@ -1,15 +1,11 @@
+"use strict";
 
-// const locationsArray = require("./locationData.js");
-// const parkTypesArray = require('./parkTypeData.js');
-import {locationArray} from "./locationData.js";
-import {parkTypesArray} from "./parkTypeData.js";
-import {nationalParksArray} from "./nationalParkData.js";
-  // const locationArray1 = require("./locationData.js");
-//   require(['./locationData.js'], function (foo) {
-//     //foo is now loaded.
-// });
-// console.log(locationArray);
+// Import data arrays
+import { locationArray } from "./locationData.js";
+import { parkTypesArray } from "./parkTypeData.js";
+import { nationalParksArray } from "./nationalParkData.js";
 
+// DOM element references
 const searchSection = document.getElementById("search-section");
 const inputSearch = document.getElementById("input-search");
 const searchInput = document.getElementById("search-input");
@@ -22,135 +18,94 @@ const resultSection = document.getElementById("results-section");
 const myParks = document.getElementById("my-parks");
 const searchLabel = document.getElementById("search-label");
 
-  // Event listeners for radio button changes
-  searchLocationRadio.addEventListener("change", toggleSearchOptions);
-  searchTypeRadio.addEventListener("change", toggleSearchOptions);
-  searchInput.addEventListener("change", updateDropdown);
- 
-  // Event listener for search button click
-  inputSearchButton.addEventListener("click", () => {
-    let filteredParks = [];
+// Event listeners
+searchLocationRadio.addEventListener("change", toggleSearchOptions);
+searchTypeRadio.addEventListener("change", toggleSearchOptions);
+searchInput.addEventListener("change", updateDropdown);
+inputSearchButton.addEventListener("click", () => {
+  let filteredParks = [];
 
-    // Determine filter criteria based on radio button selection
-    if (searchLocationRadio.checked) {
-        filteredParks = nationalParksArray.filter(park => park.State === location);
+  if (searchLocationRadio.checked) {
+    // Filtering parks based on selected location
+    filteredParks = nationalParksArray.filter(park => park.State === location);
 
-    } else if (searchTypeRadio.checked) {
-        filteredParks = nationalParksArray.filter(park => park.ParkType === parkType);
+  } else if (searchTypeRadio.checked) {
+    // Filtering parks based on selected park type
+    filteredParks = nationalParksArray.filter(park => park.ParkType === parkType);
+  }
+
+  // Displaying filtered parks
+  displayResults(filteredParks);
+});
+
+// Populating dropdown
+populateDropdown(searchInput, locationArray);
+
+// Populating the dropdown menu 
+function populateDropdown(inputSearch, dataArray) {
+  inputSearch.innerHTML = "";
+  dataArray.forEach(item => {
+    const option = document.createElement("option");
+    option.value = item;
+    option.text = item;
+    inputSearch.appendChild(option);
+    if (option.index == 0) {
+      option.selected = 'true';
+      getParks(item);
     }
-
-    // Display filtered parks
-    displayResults(filteredParks);
   });
+}
 
-  // Initialize dropdown with unique states on page load
-//  populateDropdown(searchInput, getUniqueStates(nationalParksArray));
-  populateDropdown(searchInput,locationArray);
+// Search options based on radio button
+function toggleSearchOptions() {
+  if (searchLocationRadio.checked) {
+    searchLabel.textContent = "Choose State:";
+    populateDropdown(searchInput, locationArray);
+    parkTemplate(inputSearch.value);
+  } else if (searchTypeRadio.checked) {
+    searchLabel.textContent = "Choose Park Type:";
+    populateDropdown(searchInput, parkTypesArray);
+  }
+}
 
+// Update dropdown menu
+function updateDropdown() {
+  getParks(searchInput.value);
+}
 
-  // Function to populate dropdown with options
-  // function populateDropdown(dropdown, dataArray) {
-  //   dropdown.innerHTML = ""; // Clear existing options
-  //   dataArray.forEach(item => {
-  //       const option = document.createElement("option");
-  //       option.value = item;
-  //       option.textContent = item;
-  //       dropdown.appendChild(option);
-  //   });
-  // }
-    function populateDropdown(inputSearch, dataArray) {
-      inputSearch.innerHTML = ""; // Clear existing options
-      dataArray.forEach(item => {
-        const option = document.createElement("option");
-        option.value = item;
-        option.text = item;
-        inputSearch.appendChild(option);
-        if(option.index == 0){
-          option.selected = 'true';
-          getParks(item);
-        }
-    });
-  } 
-
-  // Function to toggle search options based on radio button selection
-  // function toggleSearchOptions() {
-  //   if (searchLocationRadio.checked) {
-  //       searchLabel.textContent = "Choose State:";
-  //       populateDropdown(searchInput, getUniqueStates(nationalParksArray));
-  //   } else if (searchTypeRadio.checked) {
-  //       searchLabel.textContent = "Choose Park Type:";
-  //       populateDropdown(searchInput, parkTypesArray);
-  //   }
-  // }
-  function toggleSearchOptions() {
+// Fetch parks
+function getParks(selectedItem) {
+  myParks.innerHTML = "";
+  nationalParksArray.forEach(item => {
     if (searchLocationRadio.checked) {
-        searchLabel.textContent = "Choose State:";
-        populateDropdown(searchInput,locationArray);
-        parkTemplate(inputSearch.value);
-    } else if (searchTypeRadio.checked) {
-        searchLabel.textContent = "Choose Park Type:";
-        populateDropdown(searchInput,parkTypesArray);
-    }
-  }
-
-  function updateDropdown(){
-    getParks(searchInput.value);
-    //inputSearch.selected = 'true';
-  }
-
-  function getParks(selectedItem){
-    myParks.innerHTML = "";
-    nationalParksArray.forEach(item => {
-      if(searchLocationRadio.checked){
-        if(item["State"] == selectedItem){
-          parkTemplate(item);
-        }
-      }else if(searchTypeRadio.checked){
-        selectedItem = selectedItem.split(" ").pop();
-        if(item["LocationName"].split(" ").includes(selectedItem)){
-          parkTemplate(item);
-        }
+      if (item["State"] == selectedItem) {
+        parkTemplate(item);
       }
-        // for(let key in park){
-        //   if(park["State"] == "Alabama"){
-        //     parkTemplate(park);
-        //   }
-        // }
-      // park.forEach(item => {
-      //   if(item.State == selectedItem){
-      //     parkTemplate(park);
-      //   }
-      // });
-      //else
-    });
-  }
+    } else if (searchTypeRadio.checked) {
+      selectedItem = selectedItem.split(" ").pop();
+      if (item["LocationName"].split(" ").includes(selectedItem)) {
+        parkTemplate(item);
+      }
+    }
+  });
+}
 
-//display search by location results
+// Function for searching parks by location
 function searchByLocation() {
   let message = `${locationsArray.length} National Park to visit`;
   message += locationsArray.map(parkTemplate).join("");
   document.getElementById("myParks").innerHTML = message;
 }
 
-//display search by type results
+// Function for searching parks by type
 function searchByType() {
   let message = `${parkTypesArray.length} National Park to visit`;
   message += parkTypesArray.map(parkTemplate).join("");
   document.getElementById("myParks").innerHTML = message;
 }
 
-//display park info
+// Template for displaying park information
 function parkTemplate(park) {
-  // return `
-  //       <div class="card" style="width: 18rem;">
-  //       <img src="${park.Image}" class="card-img-top" alt="...">
-  //       <div class="card-body">
-  //           <h5 class="card-title">${park.LocationName}</h5>
-  //           <p class="card-text">${park.State}.</p>
-  //           <a href="#" class="btn btn-primary">Go somewhere</a>
-  //       </div>
-  //       </div>`;
-
   const card = `
     <div class="card" style="width: 18rem;">
       <div class="card-body">
@@ -160,22 +115,20 @@ function parkTemplate(park) {
         <p class="card-text">${park.City || ''}, ${park.State || ''} ${park.ZipCode || ''}</p>
         <p class="card-text">Phone: ${park.Phone || ''}</p>
         <p class="card-text">Fax: ${park.Fax || ''}</p>
-        ${park.Visit ? `<a href="${park.Visit}" class="btn btn-primary">Visit page</a>` : ''}
+        ${park.Visit ? `<a href="${park.Visit}" class="btn btn-primary" target="_blank">Visit page</a>` : ''}
       </div>
     </div>`;
-    myParks.innerHTML += card;
+  myParks.innerHTML += card;
 }
 
-// Function to display all national parks
+// Function for viewing all parks
 function viewAllParks() {
-  // Clear existing content
   myParks.innerHTML = "";
-
-  // Display all national parks
   nationalParksArray.forEach(park => {
-      parkTemplate(park);
+    parkTemplate(park);
   });
 }
 
-// Add an event listener for "View All" button click
+// Event listener for viewing all parks button
 document.getElementById("view-all-button").addEventListener("click", viewAllParks);
+
